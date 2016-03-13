84b640be97bc50eae5d9b1b3153f7da0dcdc4c4c
Martin Begleiter <martin.begleiter@gmail.com>
2016-03-13 19:29:36 +0100

    Introduction to closures
    This commit introduces closures which are executable blocks of code. It shows how we can
    parameters to closures, how the last statement of a closure is it's return value and how
    "it" is a default parameter when no other parameter is defined. It also shows how
    closures can bind variables in the scope in which they are defined, which a very
    important characteristic of closures and how we can send closures as parameters to
    other closures.

--- src/test.groovy
+++ src/test.groovy
@@ -2,12 +2,49 @@
  * Created by martinbegleiter on 13/03/16.
  */
 
-def variable = 5
+def resistance = 10
 
-println variable.getClass()
+def current = 10
 
-variable = "Hello world"
+// Example of closure
+// Note that you can define parameters and that you do not need a return statement.
+// The last line of the closure is evaluated and returned as the result if no return
+// statement is written.
+def voltage = {res, cur -> res * cur}
 
-println variable.getClass()
 
+// Example of how a closure can use variables defined in the scope in which the closure is defined
+def voltageUsingScope = {
+    resistance * current
+}
+
+// Shows how to "call" a closure by using ()
+// Also shows how to include variables in GStrings
+def closureResult = voltage(resistance, current)
+println "The voltage is $closureResult"
+
+def scopeClosureResult = voltageUsingScope()
+println "The voltage using scope is $scopeClosureResult}"
+
+
+// Shows that every closure has it as a default parameter if no other parameter is named
+def noParameterClosure = {
+    println "I got $it"
+}
+noParameterClosure()
+noParameterClosure("Hello")
+
+
+// This shows how we can define a closure and then, since it is a first class citizen, send it in as a parameter to
+// another closure
+def transform = {x,y ->
+    2*x + y
+}
+
+def applyTransformToParams(trans, first, second) {
+    def result = trans(first, second)
+    println "The result of the transform is = $result"
+}
+
+applyTransformToParams(transform, 4, 5)
 
